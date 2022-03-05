80c2befec95babf673dd6fe34dc0e48bb871425e
TitikUniverse <titikuniverse@gmail.com>
2022-03-05 16:23:21 +0300

    Initial commit

---
+++ DataAccess.Core/Interfaces/DbContext/IDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Chillout.DataAccess.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chillout.DataAccess.Core.Interfaces.DbContext
+{
+    public interface IDbContext : IDisposable, IAsyncDisposable
+    {
+        DbSet<UserRto> Users { get; set; }
+        DbSet<HistoryGameRto> HistoryGames { get; set; }
+        Task<int> SaveChangesAsync(CancellationToken cancelationToken = default);
+    }
+}
---
+++ DataAccess.Core/Models/HistoryGameRto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Chillout.DataAccess.Core.Models
+{
+    [Table("HistoryGame")]
+    public class HistoryGameRto
+    {
+        public int Id { get; set; }
+
+        public int LeftUserId { get; set; }
+        public UserRto LeftUser { get; set; }
+
+        public int RightUserId { get; set; }
+        public UserRto RightUser { get; set; }
+
+        public int WinnerUserId { get; set; }
+        public UserRto WinnerUser { get; set; }
+
+        public int AdditionMoney { get; set; }
+        public DateTime DateTimeStamp { get; set; }
+    }
+}
