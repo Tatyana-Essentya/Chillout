008868b3740e855fd31e8b3126b5a47ef99cdd11
Sashagusew <rzhuludkov@bk.ru>
2022-03-05 17:46:17 +0300

    Add UserInventory
    created a new UserInventory class

---
+++ Chillout/DataAccess.Core/Models/UserInventory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chillout.DataAccess.Core.Models
+{
+    [Table("User")]
+    public class UserInventory
+    {
+        public int id { get; set; }
+        public string nickname { get; set; }
+        public string thing { get; set; }
+        public string QuantityThing { get; set; }
+        public string TypeThing { get; set; }
+
+        public List<UserRto> UserRto { get; set; }
+    }
+}
